cf9ee95ab64f9427aba74a905e5bc18bd7c95a09
codewithadity <mradityaraj22@gmail.com>
2021-09-08 20:30:46 +0530

    Checked letter case

--- main.py
+++ main.py
@@ -2,5 +2,15 @@ import json
 
 data= json.load(open("data.json"))
 
-print("The definition d of the word is")
-print(data["rain"])
+
+def translate(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    else:
+        return f"The word {word} doesn't exist.Please double check it."
+
+
+
+input_word=input("Enter a existing word: ")
+print(translate(input_word))
