9ac3b0e9adc9a332c923aa5733013f8b78c19321
codewithadity <mradityaraj22@gmail.com>
2021-09-09 19:46:51 +0530

    Version 1.0

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+from difflib import get_close_matches
+
 
 data= json.load(open("data.json"))
 
@@ -7,10 +9,24 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word,data.keys())) >0:
+        confirmation=input(f"Did you mean {get_close_matches(word,data.keys())[0]} instead? Y or N?: ").lower()
+        if confirmation=="y":
+            return data[get_close_matches(word,data.keys())[0]]
+        elif confirmation=="n":
+            return f"The word {word} doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
     else:
-        return f"The word {word} doesn't exist.Please double check it."
+        return f"The word {word} doesn't exist. Please double check it."
 
 
 
 input_word=input("Enter a existing word: ")
-print(translate(input_word))
+output= translate(input_word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
